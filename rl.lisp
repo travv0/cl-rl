22b5ef5bf7a27738a0233dc270e992639e5d934b
Travis <tssund93@gmail.com>
2020-04-27 20:38:06 -0500

    player movement

--- rl.lisp
+++ rl.lisp
@@ -7,12 +7,18 @@
 (defcomponent position (x y z))
 (defcomponent velocity (x y z))
 
+(defcomponent friction (amount))
+
 (defcomponent visible (char color))
 
 (defsys move ((position velocity) (e))
   (incf (position/x e) (velocity/x e))
   (incf (position/y e) (velocity/y e))
   (incf (position/z e) (velocity/z e)))
+
+(defsys apply-friction ((velocity) (e))
+  (decf (velocity/x e) (* (velocity/x e) (friction/amount e)))
+  (decf (velocity/y e) (* (velocity/y e) (friction/amount e))))
 
 (defvar *display-function*
   (lambda (x y char color)
@@ -27,17 +33,31 @@
            (visible/color e)))
 
 (defparameter *player*
-  (add-entity nil
-    (position :x 0 :y 0 :z 0)
-    (velocity :x 1 :y 1 :z 0)
-    (visible :char #\@ :color :white)))
+  (progn (when *player*
+           (remove-entity *player*))
+         (add-entity nil
+           (position :x 50 :y 50 :z 0)
+           (velocity :x 0 :y 0 :z 0)
+           (friction :amount 1)
+           (visible :char #\@ :color :white))))
 
 (defparameter *systems*
   '(move
+    apply-friction
     display)
   "a list of all system names in the order they should run")
 
 (defun update (display-function action)
-  (format t "Got action: ~a~%" action)
+  (case action
+    (:move-left (setf (velocity/x *player*) -1))
+    (:move-up (setf (velocity/y *player*) -1))
+    (:move-right (setf (velocity/x *player*) 1))
+    (:move-down (setf (velocity/y *player*) 1))
+    (:move-up-left (setf (velocity/x *player*) -1) (setf (velocity/y *player*) -1))
+    (:move-up-right (setf (velocity/x *player*) 1) (setf (velocity/y *player*) -1))
+    (:move-down-left (setf (velocity/x *player*) -1) (setf (velocity/y *player*) 1))
+    (:move-down-right (setf (velocity/x *player*) 1) (setf (velocity/y *player*) 1))
+    (t (cerror "continue" "unknown action: ~a" action)))
+
   (let ((*display-function* display-function))
     (mapc #'do-system *systems*)))
