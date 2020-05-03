d3cefe2811445441c8af60b44b6e1b6338709868
Travis <tssund93@gmail.com>
2020-05-02 23:02:14 -0500

    improved enemy ai a bit

--- rl/package.lisp
+++ rl/package.lisp
@@ -6,6 +6,7 @@
                 #:when-let
                 #:when-let*
                 #:curry
+                #:rcurry
                 #:removef
                 #:lastcar)
   (:import-from #:serapeum
